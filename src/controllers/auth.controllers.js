bbbb43b6c329d28f00acdbe6f213eb6462f2b050
Naman Mehrotra <namanlinkedin183@gmail.com>
2026-07-30 11:57:00 +0530

    Improve signup API with try catch handling

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,34 +1,49 @@
 const User = require("../models/user.models");
+const bcrypt = require("bcrypt");
 
 const signup = async (req, res) => {
-  const { name, email, password, username } = req.body; // We destructured, instead of prinitng the entire object
+  try {
+    const { name, email, password, username } = req.body; // We destructured, instead of prinitng the entire object
 
-  //Validating the user details
-  if (!name || !email || !password || !username) {
-    return res.status(400).json({
-      message: "All fields are required",
+    //Validating the user details
+    if (!name || !email || !password || !username) {
+      return res.status(400).json({
+        message: "All fields are required",
+      });
+    }
+
+    //Checking duplicate user
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+    // Hashing the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword, // Changed the password to password:hashedPassword after hashing
+      username,
+    });
+
+    await user.save();
+
+    return res.status(201).json({
+      success: true,
+      message: "Signup successful",
+    });
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
     });
   }
-  
-  //Checking duplicate user
-  const existingUser = await User.findOne({email}) 
-
-  if(existingUser) {
-    return res.json(400).json({
-      message: "User already exists"
-    });
-  }
-  
-  const user = new User({
-    name,
-    email,
-    password,
-    username,
-  });
-
-  await user.save();
-
-  res.send("Signup successful");
 };
 
 const login = (req, res) => {
